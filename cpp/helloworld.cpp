2c2f31ab3c5e158c08059fb799304ee3554c76f9
Faisal Hameed <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-21 22:50:22 +0500

    i hate cpp

---
+++ cpp/helloworld.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+    cout << "Hello, World!" << endl;
+
+    // Let's add some more lines to show basic output
+    cout << "This is my first C++ program!" << endl;
+    cout << "Learning how to use cout for output." << endl;
+
+    // Using variables
+    string name = "Faisal";
+    cout << "Welcome, " << name << "!" << endl;
+
+    return 0;
+}
